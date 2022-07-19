44d101390f11c38eb52e1419e655f06ee1ce7e99
RedTTG <redtonehair@gmail.com>
2022-07-19 06:32:49 +0300

    Updated call functions example
    All call functions as of the version have been added to demonstrate them in action!

--- hexicapi-example/client.py
+++ hexicapi-example/client.py
@@ -1,19 +1,42 @@
 import hexicapi.client as client
 
+# Below is a demonstration of all the different call functions
 @client.on_calf
-def authentication_fail(reason):
-    print("authentication error")
-    print(reason)
-    exit()
+def connecting(reason): print(f'Connecting: {reason}')
+
 @client.on_calf
 def connection_fail(reason):
-    print('connection error')
-    print(reason)
+    print(f'Connection error: {reason}')
     exit()
+
+@client.on_calf
+def connection_success(reason): print(f'Connection successful: {reason}')
+
+@client.on_calf
+def authenticating(reason): print(f'Authenticating: {reason}')
+
+@client.on_calf
+def authentication_fail(reason):
+    print(f'Authentication error: {reason}')
+    exit()
+
+@client.on_calf
+def authentication_success(reason): print(f'Authentication successful: {reason}')
+
+@client.on_calf
+def handshake(reason): print(f'Handshake: {reason}')
+
 @client.on_calf
 def disconnect(reason):
-    print('closing due to disconnect...')
-    print(reason)
+    print(f'Closing due to disconnect... {reason}')
+    exit()
+
+@client.on_calf
+def heartbeat(reason): print(f'Heartbeat: {reason}')
+
+@client.on_calf
+def heartbeat_error(reason):
+    print(f'Heartbeat error: {reason}')
     exit()
 
 client.ip = "127.0.0.1" # Set the server ip
